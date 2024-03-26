403a504ec913c5788f2190dd4d159932e3fe17da
Ariel Lesmana <edselhanswijaya@gmail.com>
2024-03-26 22:29:31 +0700

    update orders page

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -21,27 +21,9 @@ const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
-  const formattedOrders: OrderColumn[] = orders.map((item) => ({
-    id: item.id,
-    phone: item.phone,
-    address: item.address,
-    products: item.orderItems
-      .map((orderItem) => orderItem.product.name)
-      .join(','),
-    totalPrice: formatter.format(
-      item.orderItems.reduce((total, item) => {
-        return total + Number(item.subtotal);
-      }, 0),
-    ),
-    isPaid: item.isPaid,
-    createdAt: format(item.createdAt, 'dd MMMM yyyy'),
-  }));
-
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <OrderClient data={formattedOrders} />
-      </div>
+      <div className="flex-1 space-y-4 p-8 pt-6">Something</div>
     </div>
   );
 };
