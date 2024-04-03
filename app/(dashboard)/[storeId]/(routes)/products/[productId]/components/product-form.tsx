857170b9d70ef65cce31b4ecd9f5bc32f13e71b9
Edsel Hans <edselhanswijaya@gmail.com>
2024-04-03 20:20:07 +0700

    fix ts error

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -4,7 +4,7 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Product, Category, Image, Size } from '@prisma/client';
+import { Product, Category, Image, Size, Supplier } from '@prisma/client';
 import { Trash } from 'lucide-react';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
