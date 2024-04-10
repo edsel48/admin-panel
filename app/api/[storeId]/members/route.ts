feb17521641e6b50d6d0965a63cd961cf4567036
Edsel Hans <edselhanswijaya@gmail.com>
2024-04-10 12:29:31 +0800

    Created Member module and POST for creating member

---
+++ app/api/[storeId]/members/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { name, limit, username, password } = body;
+
+    if (!name) {
+      return new NextResponse('Name is Required', { status: 400 });
+    }
+
+    const member = prismadb.member.create({
+      data: {
+        name,
+        limit,
+        username,
+        password,
+      },
+    });
+
+    return NextResponse.json(member);
+  } catch (error) {
+    console.log('[MEMBERS_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
