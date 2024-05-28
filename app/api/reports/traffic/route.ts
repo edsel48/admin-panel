dc9b5995a4d7a717974a95b89f8ef0c775ea96c1
Edsel Hans <edselhanswijaya@gmail.com>
2024-05-28 23:28:51 +0700

    update cors

--- app/api/reports/traffic/route.ts
+++ app/api/reports/traffic/route.ts
@@ -4,6 +4,16 @@ import { NextResponse } from 'next/server';
 
 import { format } from 'date-fns/format';
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function GET(req: Request) {
   let traffics = await prismadb.traffic.findMany({});
 
